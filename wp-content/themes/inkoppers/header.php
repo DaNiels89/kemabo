5998df0a26dc19b3a9cb11a98b8fac00e55ef099
Niels Slo <nielssloesen@hotmail.com>
2021-08-10 11:39:23 +0200

    Updated to Bootstrap v4.6.0

--- wp-content/themes/inkoppers/header.php
+++ wp-content/themes/inkoppers/header.php
@@ -15,14 +15,14 @@ defined( "ABSPATH" ) || exit;
 <?php do_action("wp_body_open"); ?>
 
 <nav id="mobile-menu" class="navbar navbar-expand-lg navbar-light bg-white">
-  <a class="navbar-brand" href="<?php echo home_url(); ?>">
-    <img src="/kemabo/wp-content/uploads/2021/08/logo.svg" width="152,37" height="30,5" class="d-inline-block align-top" alt="">
-  </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="custom-toggler navbar-toggler-icon"></span>
-  </button>
+    <a class="navbar-brand" href="<?php echo home_url(); ?>">
+      <img src="/kemabo/wp-content/uploads/2021/08/logo.svg" width="152,37" height="30,5" class="d-inline-block align-top" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="custom-toggler navbar-toggler-icon"></span>
+    </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <?php wp_nav_menu(['menu' => 'Hoofdmenu', 'container' => false, 'menu_class' => false]); ?>
-  </div>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <?php wp_nav_menu(['menu' => 'Hoofdmenu', 'container' => false, 'menu_class' => false]); ?>
+    </div>
 </nav>
